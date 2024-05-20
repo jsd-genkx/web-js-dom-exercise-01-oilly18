532ef025c314d954d58437ade92053d1fc64d8c9
oilly18 <oilly18@hotmail.com>
2024-05-20 13:46:00 +0700

    firstcommit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeHello() {
+   let elementH1 = document.getElementById("mytext")
+   elementH1.innerHTML = "Hello Javascript InnerHTML!!";
+}
+
+function changeHi() {
+    let elementH2 = document.getElementById("mytext2")
+    elementH2.innerHTML = "Hello Oil!!";
+ }
